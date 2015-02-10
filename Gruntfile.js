2dd84d20d3d6649c1e4989fca41bdc1f3a365e39
dnvy <dnvy0084@gmail.com>
2015-02-10 15:20:01 +0900

    commited by grunt watch at 2015-02-10, 03:20:01 PM

---
+++ Gruntfile.js
@@ -0,0 +1,66 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        // uglify 설정. 
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> [<%= pkg.version %>] <%= grunt.template.today("yyyy-mm-dd, hh:MM:ss TT") %> */\n'
+            },
+            build: {
+                src: 'build/<%= pkg.name %>.js',
+                dest: 'js/<%= pkg.name %>.min.js'
+            }
+        },
+
+        shell: 
+        {
+            options: {
+                stdout: true,
+                stderr: true
+            },
+
+            target: {
+                command: [ 
+                    'git add .',
+                    'git commit -m "commited by grunt watch at <%= grunt.template.today("yyyy-mm-dd, hh:MM:ss TT") %>"'
+                ].join( '&' )
+            }
+        },
+
+        // concat 설정. 
+        concat: {
+            basic: {
+                src: [
+                    "src/*.js" 
+                ],
+
+                dest: "build/<%= pkg.name %>.js" 
+            }
+        },
+
+        watch: {
+            scripts: {
+                files: ['src/*.js'],
+                tasks: ['concat', 'uglify', 'shell']
+            },
+        }
+    });
+
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // Load the plugin that provides the "concat" task.
+    grunt.loadNpmTasks( 'grunt-contrib-concat');
+
+    // load watch
+    grunt.loadNpmTasks( 'grunt-contrib-watch');
+
+    // load shell
+    grunt.loadNpmTasks( 'grunt-shell');
+
+    // Default task(s)
+    grunt.registerTask('default', ['concat', 'uglify']);
+
+};
